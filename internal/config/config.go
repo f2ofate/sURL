46f6101408aa4621a5fcb4dd93cf051f617a02b5
f2ofate <gungerfree@gmail.com>
2025-07-01 02:07:00 +0300

    ref | read config from os envs

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,22 +3,24 @@ package config
 import (
 	"flag"
 	"fmt"
-	"log/slog"
+	"log"
 	"net/http"
 	"sURL/internal/api"
 	"sURL/internal/storage"
+
+	"github.com/caarlos0/env/v11"
 )
 
 type Config struct {
 	Storage          storage.Repository
-	BasicResaultAddr string
-	Address          string
+	BasicResaultAddr string `env:"BASE_URL"`
+	Address          string `env:"SERVER_ADDRESS"`
 }
 
 func (c *Config) Run() {
 	fmt.Printf("Server address: %s\n\rBasic resault address: %s\n\r", c.Address, c.BasicResaultAddr)
 	if err := http.ListenAndServe(c.Address, api.NewRouter(c.Storage, c.BasicResaultAddr)); err != nil {
-		slog.Error(err.Error())
+		log.Fatal(err.Error())
 	}
 }
 
@@ -32,6 +34,10 @@ func Configure(storage storage.Repository) *Config {
 	flag.StringVar(&config.BasicResaultAddr, "b", "http://localhost:8080", "The server basic redirect url")
 	flag.StringVar(&config.Address, "a", ":8080", "The server Address and port")
 	flag.Parse()
+
+	if err := env.Parse(config); err != nil {
+		log.Fatal(err.Error())
+	}
 
 	return config
 }
