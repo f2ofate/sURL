ade7a90d1bcbe67ba4f3dfeb3d2c83d31e42625d
f2ofate <gungerfree@gmail.com>
2025-07-01 22:17:56 +0300

    ref | receive req in json; response res in json

--- internal/api/v1/handlers.go
+++ internal/api/v1/handlers.go
@@ -1,7 +1,7 @@
 package v1
 
 import (
-	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
 	"sURL/internal/storage"
@@ -9,9 +9,19 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type requset struct {
+	URL string `json:"url"`
+}
+
+type response struct {
+	Result string `json:"result"`
+}
+
 // StoreURL получает тело запроса и сохраняет его в хранилище
 func StoreURL(s storage.Repository, resaultAddr string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req requset
+
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, "Error reading request body: "+err.Error(), http.StatusInternalServerError)
@@ -19,17 +29,26 @@ func StoreURL(s storage.Repository, resaultAddr string) http.HandlerFunc {
 		}
 		defer r.Body.Close()
 
-		shortUrl, ok := s.AddShortURL(string(body))
+		if err = json.Unmarshal(body, &req); err != nil {
+			http.Error(w, "Error reading body: "+err.Error(), http.StatusBadRequest)
+		}
+
+		shortUrl, ok := s.AddShortURL(req.URL)
 		if !ok {
 			http.Error(w, "Invalid URL format. Must be http://... or https://...", http.StatusBadRequest)
 			return
 		}
 
-		response := fmt.Sprintf("%s/%s", resaultAddr, shortUrl)
+		res := response{Result: resaultAddr + "/" + shortUrl}
 
-		w.Header().Set("Content-Type", "text/plain")
+		jsonRes, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, "Error marshalling response body: "+err.Error(), http.StatusBadRequest)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(response))
+		w.Write(jsonRes)
 	}
 }
 
